fad759bd167e8c0d8c63bbc52cc94d817d62a610
Paolo Ceccarelli <paolo.ceccarelli@waveinformatica.com>
2023-03-06 15:17:38 +0100

    initial commit + setup

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_CSS_MANIFEST={"__entry_css__":{"/Users/paoloceccarelli/Code_Personal/React/metaverse-web/app/page":["/Users/paoloceccarelli/Code_Personal/React/metaverse-web/styles/globals.css"]},"/Users/paoloceccarelli/Code_Personal/React/metaverse-web/app/layout.js":["/Users/paoloceccarelli/Code_Personal/React/metaverse-web/styles/globals.css"]}
