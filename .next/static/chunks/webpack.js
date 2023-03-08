078d8119d9487d1ee6bbf0102f2eeb0fa9243f5c
Paolo <paolo.cecca96@gmail.com>
2023-03-08 13:24:56 +0100

    World + Insights + InsightCard sections

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4a2b1b66a2867de2"; }
+/******/ 		__webpack_require__.h = function() { return "6db696688a4aa03d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
